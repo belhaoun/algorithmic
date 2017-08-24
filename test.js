0b671a7cb094a82917bb4ed24cd6bd1caf01c565
belhaoun <badr.elhaouni@gmail.com>
2017-08-24 05:05:08 -0700

    les fonctions

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 var tools = require("./tools");
 
 console.log('La somme de 1 et 2 est : ' + tools.somme(1, 2));
-console.log('La produit de 3 et 2 est : ' + tools.multiplication(2, 3));
+console.log('Test 1 function somme : ' + tools.somme(1, 2) + ' == 3 est :', tools.somme(1, 2) == 3);
+console.log('Test 2 function somme : ', tools.somme(1, 2) == 4);
+//console.log('La produit de 3 et 2 est : ' + tools.multiplication(2, 3));
+
+console.log('La max de 1 et 2 est : ' + tools.findMax2(1, 2 ));
+
+console.log('La max de 1 et 2 et 3 est : ' + tools.findMax3(1, 2 ,5 ));
+console.log('La max de 1 et 2 et 3 est : ' + tools.findMaxTab(1, 2 ,5 ));
+console.log('La plus grande valeur est ('+ PlusGrandeValeur(tab[1,2,4]));
--- tools.js
+++ tools.js
@@ -3,7 +3,58 @@ module.exports = {
 	somme: function somme(x, y){
 		return x + y;
 	},
+	
 	multiplication: function multiplication(x, y) {
 		return x * y;
-	}
+	},
+
+	findMax2: function findMax2 (x, y) {
+
+			if ( x>y)
+		return x;
+			else
+		return y;
+	},
+
+	findMax3: function findMax3 (x, y, z) {
+	var max;
+	
+		if (x>y && x>z){
+		return max;}
+
+	
+		else if (y>x && y>z){
+		return y;}
+	
+		else if(z>x && z>y){
+		return z;}
+	},
+
+	//findMaxTab: function findMaxTab (x, y, y) {
+	//	return 'TODO';
+	//},
+
+	//function findMaxTab(NomTableau, ValeurMax)
+   //{   
+   //var ValeuMax = NomTableau[0];
+    //for (i=0; i<NomTableau.length; i++)
+      //      {
+        //        if (NomTableau[i] > ValeurMax)
+          //          {
+              ///          return ValeurMax = NomTableau[i];
+             //       }
+            //}
+//    },  
+function PlusGrandeValeur(array)
+{
+    var max;
+    for (i=0;val=array[i]; i++)
+    {
+        if (array[i] > max)
+        {
+            max=array[i];
+        }
+    }
+    return max;
+}
 }
